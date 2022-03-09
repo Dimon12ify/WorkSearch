bd37d22e63a9456d2f6caa2061e49291fa9166e5
Dimon12ify <dimon12ify@yandex.ru>
2022-03-09 09:33:07 +0500

    Initial commit

---
+++ src/main/java/com/example/worksearch/WorkSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.worksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worksearch/WorkSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worksearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
